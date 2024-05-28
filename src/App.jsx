1f2773957363d2ed6a964ee0dace6ea65c3996fc
smarttechfire <educationfire552@gmail.com>
2024-05-28 21:38:33 +0530

    Layout Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,16 @@ function App() {
       <div className=" w-64 fixed">
         <SideNav selectedIndex={(value) => console.log(value)} />
       </div>
-      <div className=" ml-64">
-        Body
+      <div className=" ml-64 grid grid-cols-1 md:grid-cols-6">
+        <div className=" md:col-span-2 bg-green-200">
+          Control Panel
+        </div>
+        <div className=" md:col-span-3 bg-red-100">
+          Icon Preview
+        </div>
+        <div className=" bg-blue-100">
+          Ads Banner
+        </div>
       </div>
     </>
   )
