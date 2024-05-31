c6418c81f635a8828fe49ecd2b66cf66ddf7bad5
smarttechfire <educationfire552@gmail.com>
2024-05-31 11:44:03 +0530

    Background Controller

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import BackgroundController from './components/BackgroundController'
-import Header from './components/Header'
-import IconController from './components/IconController'
-import SideNav from './components/SideNav'
+import { useState } from "react";
+import BackgroundController from "./components/BackgroundController";
+import Header from "./components/Header";
+import IconController from "./components/IconController";
+import SideNav from "./components/SideNav";
 
 function App() {
-const [selectedIndex,setSelectedIndex] = useState();
+  const [selectedIndex, setSelectedIndex] = useState();
 
   return (
     <>
-      <Header/>
+      <Header />
       <div className=" w-64 fixed">
         <SideNav selectedIndex={(value) => setSelectedIndex(value)} />
       </div>
       <div className=" ml-64 grid grid-cols-1 md:grid-cols-6 fixed">
         <div className=" md:col-span-2 border h-screen shadow-sm p-5 overflow-auto">
-          {
-            selectedIndex==0?
-            <IconController/>:<BackgroundController/>
-          }
-         
+          {selectedIndex == 0 ? <IconController /> : <BackgroundController />}
         </div>
-        <div className=" md:col-span-3 bg-red-100">
-          Icon Preview
-        </div>
-        <div className=" bg-blue-100">
-          Ads Banner
-        </div>
+        <div className=" md:col-span-3 bg-red-100">Icon Preview</div>
+        <div className=" bg-blue-100">Ads Banner</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/BackgroundController.jsx
+++ src/components/BackgroundController.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react"
+import { Slider } from "./ui/slider"
+import ColorPickerController from "./ColorPickerController";
+
 
 function BackgroundController() {
+
+  const [rounded, setRounded] = useState(0)
+  const [padding, setPadding] = useState(40)
+  const [color,setColor] = useState('#000');
+
+  const storageValue = JSON.parse(localStorage.getItem('value'));
+
+  useEffect(() => {
+    const updatedValue={
+      ...storageValue,
+      bgRouded: rounded,
+      bgPadding:padding,
+      bgColor: color
+    }
+    localStorage.setItem('value',JSON.stringify(updatedValue))
+  })
+
   return (
     <div>
-      BackgroundController
+      <div className=" py-2">
+        <label className=" p-2 flex justify-between items-center">Rounded <span>{rounded}</span></label>
+        <Slider defaultValue={[0]} max={512} step={1} onValueChange={(event) => setRounded(event[0])}/>
+      </div>
+      <div className=" py-2">
+        <label className=" p-2 flex justify-between items-center">Padding <span>{padding}</span></label>
+        <Slider defaultValue={[40]} max={100} step={1} onValueChange={(event) => setPadding(event[0])}/>
+      </div>
+      <div className=" py-2">
+        <label className=" p-2 flex justify-between items-center">Background Color</label>
+        <ColorPickerController hideController={false} selectedColor={(color) => setColor(color)} />
+      </div>
     </div>
   )
 }
